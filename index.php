4a2dd427681ec5df699f4a5284f31073ae223ac5
Djatmiko Ufn <cacaucuku@gmail.com>
2020-11-21 06:30:46 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi ut minus reprehenderit odit maiores blanditiis sed. Et cupiditate debitis fugiat?</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
